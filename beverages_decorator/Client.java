ce1f8bdd9f45f8e2dc26bda5102dbd09c79fe1ea
Pooja Talele <poojatalele0609@gmail.com>
2025-09-08 18:14:48 +0530

    beverages_decorator

--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -3,15 +3,15 @@ package beverages_decorator;
 public class Client {
 
 	public static void main(String[] args) {
-		
-
-		
 		Beverage coffee = new Cappuccino();
         System.out.println("Coffee");
 		System.out.println(coffee.cost());
 		
-		
+		Beverage latte = new ChocolateDecorator(new SugarDecorator(new Latte()));
+        System.out.println("Latte + Sugar + Chocolate cost: " + latte.cost());
 
+        Beverage cappuccino = new ChocolateDecorator(new SugarDecorator(new Cappuccino()));
+        System.out.println("Cappuccino + Milk + Sugar + cost: " + cappuccino.cost());
 	}
 
 }
